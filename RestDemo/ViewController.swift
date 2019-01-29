08382e2828d2e57c50e7cc75151aa23fed852658
chielbleyenbergh <chielbleynbergh@gmail.com>
2019-01-29 09:46:55 +0100

    Initial Commit

---
+++ RestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestDemo
+//
+//  Created by chiel bleyenbergh on 29/01/2019.
+//  Copyright © 2019 ehb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
