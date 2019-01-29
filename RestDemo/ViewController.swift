2cf4a117ba12684c854c1f2bb7f5c5788232d364
chielbleyenbergh <chielbleynbergh@gmail.com>
2019-01-29 11:42:17 +0100

    finish tableview to show all post

--- RestDemo/ViewController.swift
+++ RestDemo/ViewController.swift
@@ -9,14 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var items = [UserPost]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         print(PostParser.getAllPosts().count)
+        items = PostParser.getAllPosts()
     }
 
 
 }
+
+extension ViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let userPostForCell = items[indexPath.row]
+        
+        cell.textLabel!.text = userPostForCell.title
+        
+        
+        return cell
+    }
+    
+        
+    
+        
+    }
 
